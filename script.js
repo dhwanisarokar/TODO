50146bd684d687a79691ab726c63a8e85a86c2fc
dhwanisarokar <dhwanisarokar@gmail.com>
2023-05-30 01:57:59 +0530

    style the todo list

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ document.getElementById("form").addEventListener("submit", (e) => {
   const todo = document.getElementById("taskInput").value;
   console.log(todo);
 });
+
+const todoArray = [];
+
+function getTodoList(task) {
+  return `
+        <li class="list-group-item p-3">
+          <input class="form-check-input me-1" id="todo-1" type="checkbox" />
+          <label class="form-check-label" for="todo-1">
+            <strike>${task}</strike>
+          </label>
+        </li>  
+    `;
+}
